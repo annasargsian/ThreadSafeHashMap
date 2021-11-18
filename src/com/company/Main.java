9f3ab2b0ce0fc62d59adce815b482df8bf9f4ee4
Anna <sargsyan.h.anna@gmail.com>
2021-11-18 19:33:04 +0400

    My implementation of Thread safe HashMap

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ThreadSafeHashMap<Integer,Integer> map = new ThreadSafeHashMap<>();
+
+        System.out.println(map.size());
+        Runnable runnable = () -> {
+            while (map.size() < 10) {
+                map.put(RANDOM.nextInt(50),RANDOM.nextInt(10));
+            }
+        };
+
+        new Thread(runnable, "Thread1").start();
+        new Thread(runnable, "Thread2").start();
+        new Thread(runnable,"Thread3").start();
+
+
+        Thread.sleep(1000);
+
+    }
+}
---
+++ src/com/company/ThreadSafeHashMap.java
@@ -0,0 +1,119 @@
+package com.company;
+
+import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Supplier;
+
+
+public class ThreadSafeHashMap<K,V> implements Map<K,V> {
+
+    private final Map<K,V> map = new HashMap<K, V>();
+    private ReadWriteLock locked = new ReentrantReadWriteLock();
+
+
+    @Override
+    public int size() {
+        return doWithReadLock(map::size);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return doWithReadLock(map::isEmpty);
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return doWithReadLock(()->map.containsKey(key));
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return doWithReadLock(()->map.containsValue(value));
+    }
+
+    @Override
+    public V get(Object key) {
+        return doWithReadLock(()-> map.get(key));
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return doWithWriteLock(() -> {
+            V put = map.put(key,value);
+            System.out.println(Thread.currentThread().getName() + " " + map);
+            return put;
+        });
+
+    }
+
+    @Override
+    public V remove(Object key) {
+        return doWithWriteLock(()->map.remove(key));
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        locked.writeLock().lock();
+        try{
+            map.putAll(m);
+        } finally {
+            locked.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public void clear() {
+        locked.writeLock().lock();
+        try{
+            map.clear();
+        } finally {
+            locked.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return doWithWriteLock(map::keySet);
+    }
+
+    @Override
+    public Collection<V> values() {
+        return doWithReadLock(map::values);
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return doWithReadLock(map::entrySet);
+    }
+
+
+    private  <R> R doWithReadLock(Supplier<R> supplier){
+        this.locked.readLock().lock();
+        try{
+            return supplier.get();
+        } finally {
+            this.locked.readLock().unlock();
+        }
+    }
+    private <R> R doWithWriteLock(Supplier<R> supplier){
+        this.locked.writeLock().lock();
+        try{
+            return supplier.get();
+        } finally {
+            this.locked.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public String toString() {
+        locked.writeLock().lock();
+        try {
+            return "ThreadSafeHashMap{" +
+                    "map=" + map +
+                    '}';
+        } finally {
+            locked.writeLock().unlock();
+        }
+    }
+}
